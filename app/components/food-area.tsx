e642e4c560bece2349ae6f81e8650815a69ef412
Tim Canu <timothee.canu@diabeloop.fr>
2023-12-22 16:22:24 +0100

    As a dev, I want foods and cards to be used through context providers

--- app/components/food-area.tsx
+++ app/components/food-area.tsx
@@ -1,17 +1,14 @@
 import { FC } from 'react'
+import { useFoodsContext } from '@/app/providers/foods.provider'
 
-interface FoodAreaProps {
-    foodsAdded: number[]
-    amountOfFood: number
-}
-
-export const FoodArea: FC<FoodAreaProps> = ({ foodsAdded, amountOfFood }) => {
+export const FoodArea: FC = () => {
+    const { hiddenFoods, amountOfFood } = useFoodsContext()
     return (
         <div className="place-self-center h-64 w-1/3 bg-cyan-500 rounded-[50%] flex flex-row">
-            {foodsAdded.map((food, index) => {
+            {hiddenFoods.map((_, index) => {
                 return <div className="w-10 h-10 bg-sky-800 border mr-5" key={index} />
             })}
-            {[...Array(amountOfFood)].map((food, index) => {
+            {[...Array(amountOfFood)].map((_, index) => {
                 return (
                     <div
                         className=" border border-indigo-600 bg-green-600 rounded-full w-8 h-8 flex justify-center items-center"
