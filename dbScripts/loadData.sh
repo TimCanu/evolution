5fcb2e13e88c5e0cc579a2727ad2de3b1b358419
Tim Canu <timothee.canu@diabeloop.fr>
2023-12-24 17:50:03 +0100

    Added script to upload data to mongodb

---
+++ dbScripts/loadData.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+source .env.local
+mongo "mongodb+srv://$DB_USERNAME:$DB_PASSWORD@$DB_CLUSTER/$DATABASE_NAME" --eval "db.features.remove({})"
+mongoimport --uri="mongodb+srv://$DB_USERNAME:$DB_PASSWORD@$DB_CLUSTER/$DATABASE_NAME" --collection=features --file=./dbScripts/features.json --jsonArray
