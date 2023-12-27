4276dda9a30d6b025c3c4a5de3734032015071e6
Tim Canu <timothee.canu@diabeloop.fr>
2023-12-27 22:00:09 +0100

    Reformat code

--- src/lib/card.utils.ts
+++ src/lib/card.utils.ts
@@ -1,7 +1,7 @@
-// This method has been copied from
-// https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
 import { Card } from '@/src/models/card'
 
+// This method has been copied from
+// https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
 export const shuffleCards = (cards: Card[]): Card[] => {
     for (let i = cards.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1))
