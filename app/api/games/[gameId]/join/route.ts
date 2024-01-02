d81a42fe77d3e60773215ee215fe29c9ba7e7536
Tim Canu <timothee.canu@diabeloop.fr>
2024-01-02 14:10:27 +0100

    Prettier

--- app/api/games/[gameId]/join/route.ts
+++ app/api/games/[gameId]/join/route.ts
@@ -55,7 +55,7 @@ export const POST = async (request: NextRequest, { params }: { params: { gameId:
             .collection('games')
             .updateOne(
                 { _id: new ObjectId(params.gameId) },
-                { $set: { players: playersUpdated, remainingCards: gameCards } },
+                { $set: { players: playersUpdated, remainingCards: gameCards } }
             )
 
         const events: PusherEvent<PusherEventBase>[] = []
