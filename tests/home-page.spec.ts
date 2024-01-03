6c26e17950c2f720eafd0fdebfc368e5987d8323
Tim Canu <timothee.canu@diabeloop.fr>
2024-01-03 21:20:26 +0100

    Added basic test to check button to join a game

--- tests/home-page.spec.ts
+++ tests/home-page.spec.ts
@@ -7,3 +7,11 @@ test('has button to redirect to create game page', async ({ page }) => {
 
     await expect(page).toHaveURL('http://localhost:3000/games/create')
 })
+
+test('has button to redirect to join game page', async ({ page }) => {
+    await page.goto('http://localhost:3000/')
+
+    await page.getByRole('link', { name: 'Join a game' }).click()
+
+    await expect(page).toHaveURL('http://localhost:3000/games/join')
+})
