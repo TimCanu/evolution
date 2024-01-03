d05beb841235ccd10cfd143b1c3c7c87a9e589b0
Tim Canu <timothee.canu@diabeloop.fr>
2024-01-03 19:16:29 +0100

    Removed incorrect comment

--- tests/home-page.spec.ts
+++ tests/home-page.spec.ts
@@ -4,6 +4,6 @@ test('has button to redirect to create game page', async ({ page }) => {
     await page.goto('http://localhost:3000/')
 
     await page.getByRole('link', { name: 'Create a game' }).click()
-    // Expect a title "to contain" a substring.
+
     await expect(page).toHaveURL('http://localhost:3000/games/create')
 })
