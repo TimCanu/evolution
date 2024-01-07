76e9d18172bc8cd65264f5bf190e90b6defc8349
Tim Canu <timothee.canu@diabeloop.fr>
2024-01-07 16:04:33 +0100

    As a player, I want to see an image for the fertile card

--- src/lib/card.service.client.ts
+++ src/lib/card.service.client.ts
@@ -1,6 +1,7 @@
 import { Card } from '@/src/models/card.model'
 import { StaticImageData } from 'next/image'
 import longNeck from '../assets/images/long-neck.jpg'
+import fertile from '../assets/images/fertile.jpg'
 import missingImage from '../assets/images/missing-image.jpg'
 import { FeatureKey } from '@/src/enums/feature-key.enum'
 
@@ -8,6 +9,8 @@ export const getCardImage = (card: Card): StaticImageData => {
     switch (card.featureKey) {
         case FeatureKey.LONG_NECK:
             return longNeck
+        case FeatureKey.FERTILE:
+            return fertile
         default:
             return missingImage
     }
