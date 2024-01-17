bf3fbfe52d0ccdd2a2d4536b86740d727b02e12e
Tim Canu <timothee.canu@diabeloop.fr>
2024-01-17 21:14:21 +0100

    As a dev, I would like html to be more semantically correct

--- src/components/feature-layout.tsx
+++ src/components/feature-layout.tsx
@@ -15,24 +15,32 @@ export const FeatureLayout: FC<CardProps> = ({ feature, speciesId, speciesIndex,
     const { isEvolvingStage } = usePlayerStatus()
 
     return (
-        <div
+        <li
             data-testid={`s-${speciesIndex}-feature-${featureIndex}`}
             className="border border-indigo-600 w-24 h-36 ml-2 flex flex-col"
         >
-            <div className="mb-auto flex justify-between">
-                <span>{feature.name}</span>
+            <h1 className="mb-auto flex justify-between">
+                {feature.name}
                 {isEvolvingStage() && (
                     <button
-                        className="border-l border-b border-indigo-600 h-6 w-6"
+                        aria-label={`Remove feature ${feature.name} at species with index ${speciesIndex}`}
+                        className="h-6 w-6"
                         onClick={() => {
                             removeFeature(speciesId, feature.cardId)
                         }}
                     >
-                        X
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
+                            <path opacity="0.15"
+                                  d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+                                  fill="#000000" />
+                            <path
+                                d="M16 8L8 16M8.00001 8L16 16M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+                                stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
                     </button>
                 )}
-            </div>
-            <span>{feature.description}</span>
-        </div>
+            </h1>
+            <p>{feature.description}</p>
+        </li>
     )
 }
