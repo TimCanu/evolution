1bcb2acc7b4d9bd959724a85ceac19732df41b41
AudeSgn <120559546+AudeSgn@users.noreply.github.com>
2024-01-22 17:28:48 +0100

    Update CSS of the food area (#59)

--- src/components/food-area.tsx
+++ src/components/food-area.tsx
@@ -1,34 +1,42 @@
 import { FC } from 'react'
 import FoodAreaImg from '@/src/assets/images/foodArea.png'
+import FoodPlantImg from '@/src/assets/images/water-lily.png'
+import VersoCard from '@/src/assets/images/verso.jpg'
 import Image from 'next/image'
 import { useGameContext } from '@/src/providers/game.provider'
 
 export const FoodArea: FC = () => {
     const { hiddenFoods, amountOfFood } = useGameContext()
     return (
-        <div className="relative self-center">
-            <Image src={FoodAreaImg} alt="" height={150} width={150} />
-            <div className="place-self-center flex flex-row absolute top-1/2 left-1/4 right-1/4 rounded-md">
+        <>
+            <div className="self-center">
                 {hiddenFoods.map((_, index) => {
                     return (
-                        <div
-                            data-testid={`hidden-food-${index}`}
-                            className="w-10 h-10 bg-sky-800 border mr-5"
-                            key={index}
-                        />
+                        <div data-testid={`hidden-food-${index}`} className="mb-3 mr-2" key={index}>
+                            <Image src={VersoCard} alt="Hidden Card" height={60} width={50} />
+                        </div>
                     )
                 })}
-                {amountOfFood > 0 &&
-                    [...Array(amountOfFood)].map((_, index) => {
-                        return (
-                            <div
-                                data-testid="food-element"
-                                className=" border border-indigo-600 bg-green-600 rounded-full w-8 h-8 flex justify-center items-center"
-                                key={index}
-                            />
-                        )
-                    })}
             </div>
-        </div>
+            <div className="relative self-center top-10">
+                <Image
+                    src={FoodAreaImg}
+                    alt={`Number of food on the food area: ${amountOfFood}`}
+                    height={360}
+                    width={360}
+                />
+                <div className="place-self-center flex flex-row absolute top-1/4 left-[5rem] right-[5rem] flex-wrap rounded-md">
+                    {amountOfFood > 0 &&
+                        [...Array(amountOfFood)].map((_, index) => {
+                            return (
+                                <>
+                                    <div data-testid="food-element" key={index} />
+                                    <Image src={FoodPlantImg} alt="" height={35} width={35} />
+                                </>
+                            )
+                        })}
+                </div>
+            </div>
+        </>
     )
 }
